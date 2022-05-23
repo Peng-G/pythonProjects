490221ad810d73f3cf494cb03c46a1a596446ebe
Peng Gong <credencegp@gmail.com>
2022-05-23 14:13:46 +0200

    first commit

---
+++ my_math.py
@@ -0,0 +1,16 @@
+def square(x):
+    '''
+    >>> square(2)
+    4
+    >>> square(3)
+    9
+    '''
+    return x*x
+
+def product(x,y):
+    return x*y
+
+
+if __name__ == '__main__':
+    import doctest, my_math
+    doctest.testmod(my_math)
